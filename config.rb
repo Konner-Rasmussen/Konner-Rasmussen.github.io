9d83db4ec6219f0f40dbf79eabca4b186c3fec8e
konnerrasmussen <konnerdroid@gmail.com>
2015-02-25 14:41:43 -0600

    Project Reset

---
+++ config.rb
@@ -0,0 +1,23 @@
+require 'compass/import-once/activate'
+require 'compass'
+require 'breakpoint'
+require 'susy'
+# Require any additional compass plugins here.
+
+# Set this to the root of your project when deployed:
+http_path = "../"
+sass_dir = 'sass'
+css_dir = 'css'
+fonts_dir = 'fnt'
+
+# You can select your preferred output style here (can be overridden via the command line):
+# output_style = :expanded or :nested or :compact or output-style = :compressed
+
+# To enable relative paths to assets via compass helper functions. Uncomment:
+# relative_assets = true
+
+# To disable debugging comments that display the original location of your selectors. Uncomment:
+# 
+line_comments = false
+
+preferred_syntax = :sass
